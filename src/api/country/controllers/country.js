3ab2f410537617b0c613b82616d6b91f9ed2649d
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-02 22:49:10 -0700

    Create join code when a new country is created

--- src/api/country/controllers/country.js
+++ src/api/country/controllers/country.js
@@ -6,4 +6,26 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::country.country');
+module.exports = createCoreController('api::country.country', ({ strapi }) =>  ({  
+  async create(ctx) {
+    
+    let code = ''
+    let entries
+    do {
+      code = Math.random().toString(36).substring(2, 8).toUpperCase()
+      entries = await strapi.entityService.findMany('api::country.country', {        
+        filters: { join_code: code },        
+      });
+    } while(entries.length > 0)
+
+    ctx.request.body.data = {
+      ...ctx.request.body.data,
+      join_code: code ,      
+    };
+    
+    const response = await super.create(ctx);    
+    return response;
+  },
+
+  
+}));
