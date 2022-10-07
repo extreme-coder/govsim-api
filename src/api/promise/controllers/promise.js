35389a826d2ce41bc43220e27aefe2e2233eea81
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-07 14:40:58 -0700

    migrate code for promises from strapi v3 to strapi v4

--- src/api/promise/controllers/promise.js
+++ src/api/promise/controllers/promise.js
@@ -4,6 +4,24 @@
  * promise controller
  */
 
+
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::promise.promise');
+module.exports = createCoreController('api::promise.promise', ({ strapi }) =>  ({  
+  async create(ctx) {
+ 
+    const pr = ctx.request.body.data;
+
+    const law = await strapi.entityService.findOne('api::law.law', pr.law, {      
+      populate: { groups_against: true,  groups_support: true},
+    });
+
+    strapi.service('api::promise.promise').updatePartySupport(law.groups_against, pr.party, 0.8)
+    strapi.service('api::promise.promise').updatePartySupport(law.groups_support, pr.party, 1.2)   
+    
+    const response = await super.create(ctx);        
+    return response;
+  },
+
+  
+}));
--- src/api/promise/services/promise.js
+++ src/api/promise/services/promise.js
@@ -6,4 +6,25 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::promise.promise');
+module.exports = createCoreService('api::promise.promise', ({ strapi }) => ({
+  async updatePartySupport(groups, partyId, coeff) {
+    groups.map(async (group) => {
+      const blocks = await strapi.entityService.findMany('api::block.block', {      
+        filters: { demographics: group.id },        
+      });      
+      blocks.map(async (block) => {        
+        const party_support = await strapi.entityService.findMany('api::party-support.party-support', {      
+          filters: { block: block.id, party: partyId },        
+        });                
+        party_support.map(async (p) => {          
+          await strapi.entityService.update('api::party-support.party-support', p.id, {
+            data: {
+              support: Math.ceil(p.support * coeff),
+            },
+          });
+        })        
+      })
+    })
+  },
+
+}));
