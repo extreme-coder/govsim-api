0ab84cd2dfafc226e88136cbd539da20d6421250
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-07 18:26:32 -0700

    Check if a promise already exists

--- src/api/promise/controllers/promise.js
+++ src/api/promise/controllers/promise.js
@@ -12,10 +12,20 @@ module.exports = createCoreController('api::promise.promise', ({ strapi }) =>  (
  
     const pr = ctx.request.body.data;
 
+    
+
+    //check if a promise is already created with same parameters
+    const bills = await strapi.entityService.findMany('api::promise.promise', {      
+      filters: { party: pr.party, law: pr.law, name: pr.name}  
+    });
+
+    if(bills.length > 0) {
+      return ctx.badRequest('This bill already exists', {  })
+    }
+
     const law = await strapi.entityService.findOne('api::law.law', pr.law, {      
       populate: { groups_against: true,  groups_support: true},
     });
-
     strapi.service('api::promise.promise').updatePartySupport(law.groups_against, pr.party, 0.8)
     strapi.service('api::promise.promise').updatePartySupport(law.groups_support, pr.party, 1.2)   
     
