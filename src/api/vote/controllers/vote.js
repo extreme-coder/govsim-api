d1d3ee68b4e460f8424cb976fcb7d3944f08485c
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-08 14:37:37 -0700

    Add a status for promise and when a vote is called update that status

--- src/api/vote/controllers/vote.js
+++ src/api/vote/controllers/vote.js
@@ -6,4 +6,20 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::vote.vote');
+module.exports = createCoreController('api::vote.vote', ({ strapi }) =>  ({  
+  async create(ctx) {
+    
+    const vote = ctx.request.body.data
+    
+    await strapi.entityService.update('api::promise.promise', vote.promise, {
+      data: {
+        status: 'IN_VOTE',
+      },
+    });
+
+    const response = await super.create(ctx);    
+    return response;
+  },
+
+  
+}));
