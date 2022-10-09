534b8160a537cf63d1faa7f5d06478ccd4f90f34
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-09 15:01:22 -0700

    population attribute should be one to one relation with block

--- src/api/party/controllers/party.js
+++ src/api/party/controllers/party.js
@@ -10,7 +10,8 @@ module.exports = createCoreController('api::party.party', ({ strapi }) =>  ({
   async create(ctx) {        
     ctx.request.body.data = {
       ...ctx.request.body.data,
-      user: ctx.state.user.id  
+      user: ctx.state.user.id,  
+      publishedAt: new Date()  
     };
 
     const response = await strapi.entityService.create('api::party.party', ctx.request.body);
