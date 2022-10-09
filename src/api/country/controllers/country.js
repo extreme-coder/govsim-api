c2b71b3ac2f1e2455eda935398b1bae7b5f56d6b
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-08 17:16:51 -0700

    When creating country calculate the next election time

--- src/api/country/controllers/country.js
+++ src/api/country/controllers/country.js
@@ -22,11 +22,12 @@ module.exports = createCoreController('api::country.country', ({ strapi }) =>  (
       entries = await strapi.entityService.findMany('api::country.country', {        
         filters: { join_code: code },        
       });
-    } while(entries.length > 0)
+    } while(entries.length > 0)    
 
     ctx.request.body.data = {
       ...ctx.request.body.data,
       join_code: code ,      
+      next_election: Date.now() + (ctx.request.body.data.election_period * 60000)
     };
     
     const response = await super.create(ctx);    
