89fe6e20052e24e22fefb0a11354fa790587502b
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-13 16:59:13 -0400

    create own ballot

--- src/api/vote/controllers/vote.js
+++ src/api/vote/controllers/vote.js
@@ -17,7 +17,24 @@ module.exports = createCoreController('api::vote.vote', ({ strapi }) =>  ({
       },
     });
 
+
+
     const response = await super.create(ctx);    
+
+    //get party from user
+    const userId = ctx.state.user.id
+    const party = await strapi.entityService.findMany('api::party.party', {
+      filters: {user: userId, country:ctx.request.body.data.country}
+    })
+    
+    const partyId = party[0].id
+    await strapi.entityService.create('api::ballot.ballot', {data:{
+      vote: response.data.id,
+      for: true, 
+      party: partyId, 
+      publishedAt: new Date() }
+    });
+
     return response;
   },
 
