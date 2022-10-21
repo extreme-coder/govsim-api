0a94afa8b0f2a799a62ab315abe01d90bbdd885f
Aryan <aryan.singh.lamba@gmail.com>
2022-10-21 10:54:31 -0700

    API for marking messages read

--- src/api/message/controllers/message.js
+++ src/api/message/controllers/message.js
@@ -14,6 +14,26 @@ module.exports = createCoreController('api::message.message', ({ strapi }) =>  (
     })
     strapi.io.emit("message", await this.transformResponse(message).data)
     return response
+  },
+  async messagesRead(ctx) {    
+    const {from_party, to_party }= ctx.request.body.data 
+
+    const messages = await strapi.entityService.findMany('api::message.message', {
+      filters: { from_party: from_party, to_party: to_party }
+    })
+
+    //updateMany doesnt work with relations, so get all Ids to be updated and update at once
+    const response = await strapi.db.query('api::message.message').updateMany({
+      where: {
+        id: messages.map((m) => m.id)
+      },
+      data: {
+        is_read: true,
+      },
+    });
+    return(response)
+    
   }
+
 })) 
 
