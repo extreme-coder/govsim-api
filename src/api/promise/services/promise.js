0b5037fca978b66028ab9a1a6b6cc6259156d352
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-22 15:12:17 -0700

    fixed party support changes

--- src/api/promise/services/promise.js
+++ src/api/promise/services/promise.js
@@ -8,14 +8,17 @@ const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::promise.promise', ({ strapi }) => ({
   async updatePartySupport(groups, partyId, coeff) {
+    console.log(groups)
     groups.map(async (group) => {
       const blocks = await strapi.entityService.findMany('api::block.block', {      
-        filters: { demographics: group.id },        
-      });      
+        filters: { demographic: group.id },        
+      });
+      console.log(`blocks: ${blocks}`)
       blocks.map(async (block) => {        
         const party_support = await strapi.entityService.findMany('api::party-support.party-support', {      
           filters: { block: block.id, party: partyId },        
-        });                
+        });
+        console.log(party_support)
         party_support.map(async (p) => {          
           await strapi.entityService.update('api::party-support.party-support', p.id, {
             data: {
