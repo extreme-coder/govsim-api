20cb927105c4270d08ef9b2ae30811d16c1572c8
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-24 20:10:23 -0700

    added promotion creation effects

--- src/api/promotion/controllers/promotion.js
+++ src/api/promotion/controllers/promotion.js
@@ -6,4 +6,24 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::promotion.promotion');
+module.exports = createCoreController('api::promotion.promotion', ({ strapi }) => ({
+  async create(ctx) {
+    const response = await super.create(ctx);
+    const promotion = ctx.request.body.data;
+    const party = promotion.party
+    const promise = await strapi.entityService.findOne('api::promise.promise', promotion.promise, {      
+      populate: { law: true , party: true },
+    });
+    const law = await strapi.entityService.findOne('api::law.law', promise.law.id, {      
+      populate: { groups_against: true,  groups_support: true},
+    });
+    if (promotion.type === 'POSITIVE') {
+      strapi.service('api::promise.promise').updatePartySupport(law.groups_support, promise.party.id, (1.5 - (promotion.budget / 100 - 10)**2 / 180))
+      strapi.service('api::promise.promise').updatePartySupport(law.groups_against, promise.party.id, (0.5 + (promotion.budget / 100 - 10)**2 / 360))
+    } else {
+      strapi.service('api::promise.promise').updatePartySupport(law.groups_against, promotion.party, (1.25 - (promotion.budget / 100 - 10)**2 / 360))
+    }
+
+    return response;
+  }
+}));
