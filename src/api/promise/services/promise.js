e7d9cae3d7c2b3efffc1a5cbc23cb4294b7fba01
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-10-25 18:34:17 -0700

    blocks switch preferred party based on support

--- src/api/promise/services/promise.js
+++ src/api/promise/services/promise.js
@@ -11,20 +11,32 @@ module.exports = createCoreService('api::promise.promise', ({ strapi }) => ({
     console.log(groups)
     groups.map(async (group) => {
       const blocks = await strapi.entityService.findMany('api::block.block', {      
-        filters: { demographic: group.id },        
+        filters: { demographic: group.id },
+        populate: { preferred_party: true }
       });
       console.log(`blocks: ${blocks}`)
       blocks.map(async (block) => {        
         const party_support = await strapi.entityService.findMany('api::party-support.party-support', {      
-          filters: { block: block.id, party: partyId },        
+          filters: { block: block.id, party: partyId },
+          populate: {party: true}
         });
         console.log(party_support)
-        party_support.map(async (p) => {          
+        party_support.map(async (p) => {   
+          let s = Math.ceil(p.support * coeff)
           await strapi.entityService.update('api::party-support.party-support', p.id, {
             data: {
-              support: Math.ceil(p.support * coeff),
+              support: s,
             },
           });
+          console.log(block)
+          if (s > block.highest_support) {
+            await strapi.entityService.update('api::block.block', block.id, {
+              data: {
+                highest_support: parseInt(s),
+                preferred_party: p.party.id
+              },
+            });
+          }
         })        
       })
     })
