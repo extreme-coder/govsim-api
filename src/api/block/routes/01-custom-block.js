9aa3bab8dc04fa118dc13f55783f9502a239dd82
Aryan Singh <aryan.singh.lamba@gmail.com>
2022-12-25 21:10:53 -0800

    Wind stat proxy API

--- src/api/block/routes/01-custom-block.js
+++ src/api/block/routes/01-custom-block.js
@@ -4,6 +4,11 @@ module.exports = {
       method: 'GET',
       path: '/blocks/groups', // Only match when the URL parameter is composed of lowercase letters
       handler: 'block.blocksGroup',
+    },
+    { // Path defined with a regular expression
+      method: 'GET',
+      path: '/wind', // Only match when the URL parameter is composed of lowercase letters
+      handler: 'block.wind',
     }
   ]
 }
